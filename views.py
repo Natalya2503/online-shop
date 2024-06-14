4d249eceba2be2872b1dce9c3abdda7f9ed578c0
Natalya2503 <147742964+Natalya2503@users.noreply.github.com>
2024-06-14 19:55:08 +0300

    Add files via upload

--- views.py
+++ views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
 from users.forms import UserLoginForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 
 
 class LoginUser(LoginView):
@@ -13,6 +14,8 @@ class LoginUser(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
     success_url = reverse_lazy('main:index')
+
+   
 
    
 class RegisterUser(CreateView):
@@ -28,12 +31,24 @@ class ProfileUser(LoginRequiredMixin, UpdateView):
     template_name = 'users/profile.html'
     extra_context = {'title': 'Профиль пользователя'}
 
+    def get(self,request, *args, **kvargs):
+
+     response = super().get(request, *args, **kvargs)
+     user = self.request.user
+     response.set_cookie('user', user, max_age=3600)
+     return response
+    def get_extra_content(self, **kwargs):
+     content = super().get_extra_content(**kwargs)
+     cookie = self.request.COOKIES.get('user')
+     content['cookies'] = cookie
+     return content
+   
 
     def get_success_url(self):
         return reverse_lazy('user:profile')
     
     def get_object(self, queryset=None):
-        return self.request.user
+      return self.request.user
 
 
 
